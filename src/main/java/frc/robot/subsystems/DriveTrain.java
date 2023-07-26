b82658eb0b4dd4c425b62f3bc9f9c834ed1cad4d
Maheeshah07 <mshah25@hb.edu>
2023-07-25 20:51:07 -0400

    created actual angles which we implemented in our flipped angle code

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -16,23 +16,26 @@ import frc.robot.Constants.DriveTrainConstants;
 
 public class DriveTrain extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public static CANSparkMax steer1; 
-  public static CANSparkMax drive1; 
+  public static CANSparkMax steer1;
+  public static CANSparkMax drive1;
 
-  public static CANSparkMax steer2; 
-  public static CANSparkMax drive2; 
+  public static CANSparkMax steer2;
+  public static CANSparkMax drive2;
 
-  public static CANSparkMax steer3; 
-  public static CANSparkMax drive3; 
+  public static CANSparkMax steer3;
+  public static CANSparkMax drive3;
 
-  public static CANSparkMax steer4; 
-  public static CANSparkMax drive4; 
+  public static CANSparkMax steer4;
+  public static CANSparkMax drive4;
 
-
+  public static SparkMaxAbsoluteEncoder encoder1;
+  public static SparkMaxAbsoluteEncoder encoder2;
+  public static SparkMaxAbsoluteEncoder encoder3;
+  public static SparkMaxAbsoluteEncoder encoder4;
 
   public DriveTrain() {
-  
-    //TODO: figure out the correct device IDs or reconfigure the IDs
+
+    // TODO: figure out the correct device IDs or reconfigure the IDs
     steer1 = new CANSparkMax(0, MotorType.kBrushless);
     drive1 = new CANSparkMax(0, MotorType.kBrushless);
 
@@ -45,66 +48,64 @@ public class DriveTrain extends SubsystemBase {
     steer4 = new CANSparkMax(0, MotorType.kBrushless);
     drive4 = new CANSparkMax(0, MotorType.kBrushless);
 
-    //steering encoders
-    SparkMaxAbsoluteEncoder encoder1 = steer1.getAbsoluteEncoder(Type.kDutyCycle);
-    SparkMaxAbsoluteEncoder encoder2 = steer2.getAbsoluteEncoder(Type.kDutyCycle);
-    SparkMaxAbsoluteEncoder encoder3 = steer3.getAbsoluteEncoder(Type.kDutyCycle);
-    SparkMaxAbsoluteEncoder encoder4 = steer4.getAbsoluteEncoder(Type.kDutyCycle);
+    // steering encoders
+    encoder1 = steer1.getAbsoluteEncoder(Type.kDutyCycle);
+    encoder2 = steer2.getAbsoluteEncoder(Type.kDutyCycle);
+    encoder3 = steer3.getAbsoluteEncoder(Type.kDutyCycle);
+    encoder4 = steer4.getAbsoluteEncoder(Type.kDutyCycle);
 
-    //TODO: determine if any encoder values need to be inverted 
+    // TODO: determine if any encoder values need to be inverted
     encoder1.setInverted(false);
     encoder2.setInverted(false);
     encoder3.setInverted(false);
     encoder4.setInverted(false);
 
-    //TODO: determine the position conversion factor 
+    // TODO: determine the position conversion factor
     encoder1.setPositionConversionFactor(1);
     encoder2.setPositionConversionFactor(1);
     encoder3.setPositionConversionFactor(1);
     encoder4.setPositionConversionFactor(1);
 
-    //steering pid controllers
+    // steering pid controllers
     var controller1 = steer1.getPIDController();
     var controller2 = steer2.getPIDController();
     var controller3 = steer3.getPIDController();
     var controller4 = steer4.getPIDController();
 
-    //giving the controller an encoder to read values from 
+    // giving the controller an encoder to read values from
     controller1.setFeedbackDevice(encoder1);
     controller2.setFeedbackDevice(encoder2);
     controller3.setFeedbackDevice(encoder3);
     controller4.setFeedbackDevice(encoder4);
 
-    //if the sensor value is greater than 360, it wraps the number such that the value is between 0 and 360
+    // if the sensor value is greater than 360, it wraps the number such that the
+    // value is between 0 and 360
     controller1.setPositionPIDWrappingEnabled(true);
     controller2.setPositionPIDWrappingEnabled(true);
     controller3.setPositionPIDWrappingEnabled(true);
     controller4.setPositionPIDWrappingEnabled(true);
 
-    //sets P gain for the PID loop
-    //TODO: tune the P gain  
+    // sets P gain for the PID loop
+    // TODO: tune the P gain
     controller1.setP(1);
     controller2.setP(1);
     controller3.setP(1);
     controller4.setP(1);
 
-    //gives the controller a target value that is a position. since this is in the constructor, we set the target to 0 so that the robot does not move
+    // gives the controller a target value that is a position. since this is in the
+    // constructor, we set the target to 0 so that the robot does not move
     controller1.setReference(0, ControlType.kPosition);
     controller2.setReference(0, ControlType.kPosition);
     controller3.setReference(0, ControlType.kPosition);
     controller4.setReference(0, ControlType.kPosition);
 
-
-
-
-
   }
 
-  public void setSpeed(double fwd, double str, double rcw){
+  public void setSpeed(double fwd, double str, double rcw) {
     double rcw_fwd = rcw * Math.sin(DriveTrainConstants.THETA);
-    double rcw_str = rcw * Math.cos(DriveTrainConstants.THETA); 
+    double rcw_str = rcw * Math.cos(DriveTrainConstants.THETA);
 
-    //calculate the forward and strafe values for each wheel
+    // calculate the forward and strafe values for each wheel
     double fwd1 = fwd - rcw_fwd;
     double str1 = str + rcw_str;
 
@@ -115,9 +116,14 @@ public class DriveTrain extends SubsystemBase {
     double str3 = str - rcw_str;
 
     double fwd4 = fwd - rcw_fwd;
-    double str4 = str - rcw_str; 
+    double str4 = str - rcw_str;
 
-    //calculate the speed and angle for each wheel
+    double currentAngle1 = encoder1.getPosition();
+    double currentAngle2 = encoder2.getPosition();
+    double currentAngle3 = encoder3.getPosition();
+    double currentAngle4 = encoder4.getPosition();
+
+    // calculate the speed and angle for each wheel
     double speed1 = Math.sqrt(fwd1 * fwd1 + str1 * str1);
     double angle1 = Math.atan2(fwd1, str1);
 
@@ -130,64 +136,78 @@ public class DriveTrain extends SubsystemBase {
     double speed4 = Math.sqrt(fwd4 * fwd4 + str4 * str4);
     double angle4 = Math.atan2(fwd4, str4);
 
-    //set all wheel speeds
-    //TODO: limit speed in case speed goes over 1
-    if (speed1 != 0){
-      drive1.set(speed1);
-      steer1.getPIDController().setReference(angle1, ControlType.kPosition);
+    double[] flipAngleAndSpeed1 = flipAngle(currentAngle1, angle1, speed1);
+    double[] flipAngleAndSpeed2 = flipAngle(currentAngle2, angle2, speed2);
+    double[] flipAngleAndSpeed3 = flipAngle(currentAngle3, angle3, speed3);
+    double[] flipAngleAndSpeed4 = flipAngle(currentAngle4, angle4, speed4);
 
-    }
-    else{
+    double flippedSpeed1 = flipAngleAndSpeed1[1];
+    double flippedAngle1 = flipAngleAndSpeed1[0];
+
+    double flippedSpeed2 = flipAngleAndSpeed2[1];
+    double flippedAngle2 = flipAngleAndSpeed2[0];
+
+    double flippedSpeed3 = flipAngleAndSpeed3[1];
+    double flippedAngle3 = flipAngleAndSpeed3[0];
+
+    double flippedSpeed4 = flipAngleAndSpeed4[1];
+    double flippedAngle4 = flipAngleAndSpeed4[0];
+
+    // set all wheel speeds
+    // TODO: limit speed in case speed goes over 1
+    if (flippedSpeed1 != 0) {
+      drive1.set(flippedSpeed1);
+      steer1.getPIDController().setReference(flippedAngle1, ControlType.kPosition);
+
+    } else {
       drive1.set(0);
-      steer1.set(0); 
+      steer1.set(0);
     }
 
-    if (speed2 != 0){
-      drive2.set(speed1);
-      steer2.getPIDController().setReference(angle2, ControlType.kPosition);
+    if (flippedSpeed2 != 0) {
+      drive2.set(flippedSpeed1);
+      steer2.getPIDController().setReference(flippedAngle2, ControlType.kPosition);
 
-    }
-    else{
+    } else {
       drive2.set(0);
-      steer2.set(0); 
+      steer2.set(0);
     }
 
-    if (speed3 != 0){
-      drive3.set(speed3);
-      steer3.getPIDController().setReference(angle3, ControlType.kPosition);
+    if (flippedSpeed3 != 0) {
+      drive3.set(flippedSpeed3);
+      steer3.getPIDController().setReference(flippedAngle3, ControlType.kPosition);
 
-    }
-    else{
+    } else {
       drive3.set(0);
-      steer3.set(0); 
+      steer3.set(0);
     }
 
-    if (speed4 != 0){
-      drive4.set(speed4);
-      steer4.getPIDController().setReference(angle4, ControlType.kPosition);
+    if (flippedSpeed4 != 0) {
+      drive4.set(flippedSpeed4);
+      steer4.getPIDController().setReference(flippedAngle4, ControlType.kPosition);
 
-    }
-    else{
+    } else {
       drive4.set(0);
-      steer4.set(0); 
+      steer4.set(0);
     }
-    //TODO: ask the motors to go to a different angle and invert the speed depending on the closest position 
+    
   }
 
-  public static void setWheelAngles(double angle1, double angle2, double angle3, double angle4){
+  public static void setWheelAngles(double angle1, double angle2, double angle3, double angle4) {
     steer1.getPIDController().setReference(angle1, ControlType.kPosition);
     steer2.getPIDController().setReference(angle2, ControlType.kPosition);
     steer3.getPIDController().setReference(angle3, ControlType.kPosition);
     steer4.getPIDController().setReference(angle4, ControlType.kPosition);
   }
 
-  public static double[] flipAngle(double currentAngle, double targetAngle, double speed){
-    double[] changedValues = new double[2]; 
+  public static double[] flipAngle(double currentAngle, double targetAngle, double speed) {
+    double[] changedValues = new double[2];
     double desiredAngle = targetAngle;
     double desiredSpeed = speed;
-    if(Math.abs(targetAngle - currentAngle) > Units.degreesToRadians(90) && Math.abs(targetAngle - currentAngle) < Units.degreesToRadians(270)){
+    if (Math.abs(targetAngle - currentAngle) > Units.degreesToRadians(90)
+        && Math.abs(targetAngle - currentAngle) < Units.degreesToRadians(270)) {
       desiredAngle += Units.degreesToRadians(180);
-      if (desiredAngle > Units.degreesToRadians(180)){
+      if (desiredAngle > Units.degreesToRadians(180)) {
         desiredAngle -= Units.degreesToRadians(360);
       }
       desiredSpeed *= -1;
